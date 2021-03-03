1f9e80d83ef6eb524a3d433e4e075ad8c8648395
hollisjamison@gmail.com <hollisjamison@gmail.com>
2021-03-03 02:57:58 -0500

    Adds function to test the repo first

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const repoName = process.env.REPONAME;
 const token = process.env.TOKEN;
 const githubUser = process.env.USERNAME;
 
+// Import Axios for HTTP requests
+const axios = require("axios");
+
 // Import shelljs to run bash (git) commands
 const shell = require("shelljs");
 
@@ -30,18 +33,33 @@ const cloneAssignment = () => {
   shell.exec("git checkout -b main");
 };
 
-const pushToStudents = () => {
+const checkRepos = () => {
+  // create for loop to check all student repos and see if they exist
+  studentList.forEach((student) => {
+    // Do an HTTP get on each students supposed repo
+    axios.get(`https://github.com/${student}/${repoName}`).then(
+      (response) => {
+        // Do nothing if you get an HTTP 200
+      },
+      (error) => {
+        console.log(`Student ${student} has not made the repo yet.`);
+      }
+    );
+  });
+};
 
+const pushToStudents = () => {
   // Create a forloop to go through each student
-  studentList.forEach(student => {
+  studentList.forEach((student) => {
     // First create a URL using the student's username and the repo name
     const studentUrl = `https://github.com/${student}/${repoName}.git`;
 
     // Force a push of the assignment main repo
     shell.exec(`git push ${studentUrl} --force`);
   });
-}
+};
 
 // Now call the functions in order
+checkRepos();
 cloneAssignment();
 pushToStudents();
--- studentList.js
+++ studentList.js
@@ -1,32 +1,28 @@
-// studentList = [
-//   "andyschachter",
-//   "annie-i0",
-//   "blofele12",
-//   "smithci1",
-//   "bellusvulpes",
-//   "Danielamoateng1989",
-//   "davepenaloza",
-//   "firizarry-coder",
-//   "gioaceto",
-//   "jeraldina",
-//   "grimmi5894",
-//   "keefers1030",
-//   "kolbycamp211",
-//   "lamsa01",
-//   "mardany126",
-//   "misheline",
-//   "oaklanz",
-//   "robinmalkus",
-//   "pathou80",
-//   "larocque-m",
-//   "markh82581",
-// ];
-
 studentList = [
-  "hollisjamison",
-  "hjamisontestuser1",
-  "hjamisontestuser2"
-]
+  "andyschachter",
+  "annie-i0",
+  "blofele12",
+  "smithci1",
+  "bellusvulpes",
+  "Danielamoateng1989",
+  "davepenaloza",
+  "firizarry-coder",
+  "gioaceto",
+  "jeraldina",
+  "grimmi5894",
+  "keefers1030",
+  "kolbycamp211",
+  "lamsa01",
+  "mardany126",
+  "misheline",
+  "oaklanz",
+  "robinmalkus",
+  "pathou80",
+  "larocque-m",
+  "markh82581",
+  "marnie-k",
+  "prazinc"
+];
 
 module.exports = {
   studentList,
