3432bb3b8242934e22f1dedb0d974881b77a2ff4
kdsbatra <kdsbatra@users.noreply.github.com>
2021-07-08 15:20:00 -0400

    allowing multiple repo push

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const {
   studentList
 } = require("./studentList");
 
+let {
+  repoDetails
+} = require("./repos.js");
+
 // Import inquirer to make shell interactive
 const inquirer = require("inquirer");
 
 // Import env file and all variables
-require("dotenv").config();
-const assignmentUrl = process.env.ASSIGNMENT;
-const repoName = process.env.REPONAME;
+const dotenv = require('dotenv')
+dotenv.config();
+
+const opt = { debug: true }
+//const assignments = JSON.parse(dotenv.parse(Buffer.from(process.env.ASSIGNMENTS), opt)["REPOS"]);
 const token = process.env.TOKEN;
-const githubUser = process.env.USERNAME;
+const githubUser = process.env.GITUSER;
 const slackToken = process.env.SLACKTOKEN;
 
 // Import Axios for HTTP requests
@@ -26,100 +32,224 @@ const {
 } = require("@slack/web-api");
 const slack = new WebClient(slackToken);
 
-const checkReposSilent = () => {
-  // create for loop to check all student repos and see if they exist
-  studentList.forEach((student) => {
-    // Do an HTTP get on each students supposed repo
-    axios.get(`https://github.com/${student.github}/${repoName}`).then(
+repoDetails = repoDetails.filter(repoDetail => !repoDetail.skip);
+
+function matchedRepoDetails(repos, selectedRepos) {
+
+  let allReposFiltered = [];
+  selectedRepos.forEach(repoName => {
+    let reposFiltered = repos.filter(repo => repo.name === repoName);
+    if (reposFiltered.length === 1) {
+      allReposFiltered.push(reposFiltered[0]);
+    } else {
+      console.log(`repo details for ${repoName} not found`);
+    }
+  })
+  return allReposFiltered;
+}
+
+const checkRepo = (student, repoName, slackAlert, callback, failureCallBack) => {
+  return axios.get(`https://github.com/${student.github}/${repoName}`)
+    .then(
       (response) => {
-        console.log(`\x1b[32m ${student.github} has made their repo!`)
+        console.log(`\x1b[32m ${student.github} has made their '${repoName}' repo!`);
+        if (callback) {
+          callback();
+        }
       },
       (error) => {
-        console.log(`\x1b[31m ${student.github} has not made the repo yet.`);
+        if (slackAlert) {
+          sendMessage(student.slack, repoName);
+        }
+        console.log(`\x1b[31m ${student.github} has not made their '${repoName}' repo yet.` + (slackAlert ? ' Alert sent.' : ''));
+        if (failureCallBack) {
+          failureCallBack();
+        }
       }
-    );
+    )
+}
+
+const checkRepos = (reposSelected, i, slackAlert) => {
+  // create for loop to check all student repos and see if they exist
+
+  const repoName = reposSelected[i].name;
+
+  console.log(`\x1b[33m ======= checking if '${repoName}' repo exists =======\n`);
+
+  let promises = [];
+  studentList.forEach((student) => {
+    // Do an HTTP get on each students supposed repo
+    promises.push(checkRepo(student, repoName, slackAlert));
+  });
+
+  // continue to next repo
+  Promise.allSettled(promises).then((results) => {
+    i++;
+    if (reposSelected[i]) {
+      checkRepos(reposSelected, i, slackAlert);
+    } else {
+      console.log(`\n\x1b[33m =============== FINISHED ==============`);
+    }
+
   });
 };
 
-const checkReposAlert = () => {
-  // create for loop to check all student repos and see if they exist
+const cloneAndPushAssignments = (allSelectedRepos, i) => {
+
+  let repoName = allSelectedRepos[i].name;
+  let repoUrl = allSelectedRepos[i].url;
+
+  console.log(`\n ==================================================`);
+  console.log(`\n===== Going to try cloning repo '${repoName}' ====`);
+
+  let { stdoutClone, stderrClone, codeClone } = shell.exec(`git clone ${repoUrl}`, { silent: true });
+  if (stderrClone) {
+    console.log(`Could not clone ${repoUrl}`, stderrClone);
+  }
+
+  // Change branch to main as this is new Github default
+  shell.cd(`${repoName}`);
+  let { stdoutCheckout, stderrCheckout, codeCheckout } = shell.exec("git checkout -b main", { silent: true });
+  if (stderrCheckout) {
+    console.log(`Could not crete and switch to main branch`, stderrCheckout);
+  }
+
+  console.log(`\n===== Going to try pushing repo '${repoName}' ====`);
+  pushRepoToStudents(repoName, allSelectedRepos[i + 1]);
+
+  shell.exec("cd ..");
+
+  // continue to next repo
+  i++
+  if (allSelectedRepos[i]) {
+    cloneAndPushAssignments(allSelectedRepos, i);
+  }
+}
+
+
+const pushRepoToStudents = (repoName, hasNext) => {
+
+  // Create a forloop to go through each student
+  let promises = [];
   studentList.forEach((student) => {
-    // Do an HTTP get on each students supposed repo
-    axios.get(`https://github.com/${student.github}/${repoName}`).then(
-      (response) => {
-        console.log(`\x1b[32m ${student.github} has made their repo!`);
-      },
-      (error) => {
-        sendMessage(student.slack);
-        console.log(`\x1b[31m ${student.github} has not made the repo yet. Alert sent.`);
+
+    promises.push(checkRepo(student, repoName, false, () => {
+      // First create a URL using the student's username and the repo name
+      const studentUrl = `https://github.com/${student.github}/${repoName}.git`;
+
+      // Force a push of the assignment main repo
+
+      let { stdout, stderr, code } = shell.exec(`git push ${studentUrl} --force`, { silent: true });
+      if (stderr) {
+        console.log(`\x1b[31m Could not push code to ${student.github} at ${studentUrl}. Error: ${stderr}`);
+        //console.log(`\x1b[31m Could not push code to ${student.github} at ${studentUrl}, Maybe they have not yet made the repo yet or you are not collabator on it`);
       }
-    );
+    }));
+
+  });
+
+  Promise.allSettled(promises).then((result) => {
+    if (!hasNext) {
+      console.log(`\n \x1b[32m ============ FINISHED pushing ${repoName} ============`);
+    }
   });
 };
 
 // Function to clone assignment locally
-const cloneAssignment = () => {
+const setupRepos = (allSelectedRepos) => {
+
   // Setup github authentication so you have access to stack education
   shell.exec(`git config --global user.name "${githubUser}"`);
   shell.exec(`git config --global user.password "${token}"`);
 
   // First remove the repo folder and remake it empty
-  shell.exec("rm -rf repo");
+  shell.rm("-rf", "repo");
   shell.exec("mkdir repo");
 
-  // Now clone the repo into that folder
+  // Now clone the repo into repo folder
   shell.cd("repo");
-  shell.exec(`git clone ${assignmentUrl}`);
 
-  // Change branch to main as this is new Github default
-  shell.cd(`${repoName}`);
-  shell.exec("git checkout -b main");
-};
-
-const pushToStudents = () => {
-  // Create a forloop to go through each student
-  studentList.forEach((student) => {
-    // First create a URL using the student's username and the repo name
-    const studentUrl = `https://github.com/${student.github}/${repoName}.git`;
-
-    // Force a push of the assignment main repo
-    shell.exec(`git push ${studentUrl} --force`);
-  });
+  cloneAndPushAssignments(allSelectedRepos, 0);
 };
 
 // Function that controls interactivity
 const promptUser = () => {
   // Prompt the user to see what kind of app needs to run
   inquirer
-    .prompt([{
-      name: "option",
-      type: "list",
-      message: "Please select your operation.",
-      choices: [
-        "1: Privately check if student repos exist. (No Slack message reminder).",
-        "2: Check if student repos exist. (With Slack message reminder).",
-        "3: Push assignment repo to students.",
-        "4: Exit application.",
-      ],
-    }, ])
+    .prompt([
+      {
+        name: "repoOptions",
+        type: "checkbox",
+        message: "Please select repos you want to check",
+        choices: ["exit", "all", new inquirer.Separator()].concat(repoDetails),
+        default: ["all"]
+      },
+      {
+        name: "option",
+        type: "list",
+        message: "Please select your operation.",
+        when: (answers) => {
+          if (answers.repoOptions == "none" || !answers.repoOptions.length) {
+            return false;
+          }
+          return true;
+        },
+        choices: [
+          "1: Privately check if student repos exist. (No Slack message reminder).",
+          "2: Check if student repos exist. (With Slack message reminder).",
+          "3: Push assignment repo to students.",
+          "4: Exit application.",
+        ],
+      },])
     // Then either check the repos or clone/push the assignment
-    .then((answer) => {
+    .then((answers) => {
+
+      let reposSelectedByName = answers.repoOptions;
+      let allReposFiltered = repoDetails;
+
+      console.log(` Repos selected: ${answers.repoOptions}\n`);
       // If option 1 is selected so a silent check.
+      if (reposSelectedByName == "exit") {
+        return;
+
+      } else if (!reposSelectedByName.length) {
+        console.log(`\x1b[33m Nothing was selected, Exiting`);
+        return;
+
+      } else if (reposSelectedByName.indexOf("all") > -1) {
+        if (reposSelectedByName.length > 1) {
+          console.log(`\x1b[33m All Repos selected, will ignore other options`);
+        }
+
+      } else {
+        let exitSelected = reposSelectedByName.indexOf("exit");
+        if (exitSelected > -1 && reposSelectedByName.length > 1) {//dont really need to check size here again
+          console.log(`\x1b[33m Exit was selected along specific repos, ignoring Exit`);
+
+          reposSelectedByName.splice(exitSelected, 1);
+        }
+
+        allReposFiltered = matchedRepoDetails(repoDetails, reposSelectedByName);
+
+        if (allReposFiltered.length !== reposSelectedByName.length) {
+          console.log(`Some repo URL is not found, found items include: [${allReposFiltered}] and repoNames selected were [${reposSelectedByName}]`)
+          return;
+        }
+      }
+
+
       if (
-        answer.option ==
-        "1: Privately check if student repos exist. (No Slack message reminder)."
-      ) {
-        checkReposSilent();
+        answers.option == "1: Privately check if student repos exist. (No Slack message reminder).") {
+        checkRepos(allReposFiltered, 0, false);
+
         // If Option 2 is selected do an alerting check.
-      } else if (
-        answer.option ==
-        "2: Check if student repos exist. (With Slack message reminder)."
-      ) {
-        checkReposAlert();
+      } else if (answers.option == "2: Check if student repos exist. (With Slack message reminder).") {
+        checkRepos(allReposFiltered, 0, true);
+
         // If option 3 is selected then clone the assignment then push.
-      } else if (answer.option == "3: Push assignment repo to students.") {
-        cloneAssignment();
-        pushToStudents();
+      } else if (answers.option == "3: Push assignment repo to students.") {
+        setupRepos(allReposFiltered);
+
         // If option 4 is selected then exit the app.
       } else {
         return;
@@ -127,12 +257,12 @@ const promptUser = () => {
     });
 };
 
-const sendMessage = (slackId) => {
+const sendMessage = (slackId, repoName) => {
   // Connect to slack and post the reminder message using the slackId provided.
   (async () => {
     const result = await slack.chat.postMessage({
       channel: slackId,
-      text: `Your repo was not found. Please create the following repo on Github. If you have created the repo please check the spelling or make the repo public. \n \n *Repo Name:* \n \`${repoName}\``,
+      text: `Your repo was not found. Please create the following repo on Github. If you have created the repo please check the spelling or make the repo public. \n \n *Repo Name:* \n \`${repoName}\` \n \n Then please add \`${githubUser}\` as a collaborator`,
     });
   })();
 };
---
+++ repos.js
@@ -0,0 +1,12 @@
+const repoDetails = [{
+        url: "https://github.com/stackeducation/fantasy-scoring.git",
+        name: "fantasy-scoring",
+        skip: false
+    },
+    {
+        url: "https://github.com/stackeducation/order-book.git",
+        name: "order-book",
+        skip: true
+    }];
+
+module.exports = {repoDetails};
