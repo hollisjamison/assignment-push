b3e3ec3a11a48a453dceb919934af7621b324f3f
kdsbatra <kdsbatra@users.noreply.github.com>
2021-07-09 18:38:48 -0400

    fix for repo check

--- studentList.js
+++ studentList.js
@@ -1,14 +1,14 @@
 const studentList = [
 	 { github: "kdsbatra", slack: "U0246KNS2KB" },
-  //    { github: "brnjackson", slack: "U023U8NK89Z" },
+  // { github: "brnjackson", slack: "U023U8NK89Z" },
   // { github: "Cpluchak", slack: "U0246LJV90R" },
-  { github: "Forensicrose", slack: "U0246LKG6E5" },
+  // { github: "Forensicrose", slack: "U0246LKG6E5" },
   // { github: "jeraldina", slack: "U0246LK6D33" },
   // { github: "kjmatt91", slack: "U0246LJTD97" },
   // { github: "katskurka", slack: "U023Z44D690" },
   // { github: "KevinSalina", slack: "U023V2RJA7P" },
   // { github: "mhafez1978", slack: "U023R04GK1B" },
-  //  { github: "RubyDF", slack: "U0246LKC43T" },
+  // { github: "RubyDF", slack: "U0246LKC43T" },
   // { github: "sherryliztynan", slack: "U0246LJAMS5" },
   
   // { github: "reinhal", slack: "U023U7T8RD1" },
